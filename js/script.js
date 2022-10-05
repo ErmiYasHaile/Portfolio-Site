9c1bcbec2cb24bca9360de260c5b02d1cb091e9f
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-10-05 16:25:05 -0700

    hamburger menu added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+// const $button=$("button")
+// const restcountries=(choice)=> {
+//     $.ajax( 
+//      {
+//          url:`https://restcountries.com/v3.1/name/${choice}`
+// })
+//  .then((data)=>{
+//      console.log(data)
+//     console.log(data[0].flags.png)
+//     //  $('#capital').text(data[0].capital);
+//     // $('#country_code').text(`${data[0].idd.root}`); 
+//     // $('#population').text(`${data[0].population}`);
+//     // $('#currencies').text(Object.keys(data[0].currencies)[0]); 
+//     // $('#region').text(data[0].region); 
+    
+//     $("img").attr("src",data[0].flags.png)
+    
+// })
+// }
+
+// $button.on("click",()=>{
+//     const $searchitem = $("#search").val() 
+//       restcountries($searchitem) 
+// })
+// .catch((error)=> {
+//     console.log(error);
+// })
+
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger= document.querySelector(".hamburger");
